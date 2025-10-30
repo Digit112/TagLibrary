f2debad160ceb7b8e97f0ca1985ed1c8bc325010
Evelyn Salie <ekobaddish@gmail.com>
2025-10-30 08:24:41 -0400

    Modified expression processing.

--- TagOperator.py
+++ TagOperator.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class TagOperator():
 	pass
 
@@ -15,61 +17,68 @@ class TagNegation(TagOperator):
 	def __init__(self, right):
 		self.right = right
 
+TagNegation.priority = 0
+TagConjunction.priority = 1
+TagDisjunction.priority = 2
+
+TagNegation.symbol = "NOT"
+TagConjunction.symbol = "AND"
+TagDisjunction.symbol = "OR"
+
+TagOperator.operations = (TagDisjunction, TagConjunction, TagNegation)
+
 class TagExpression:
 	def __init__(self, expr_str, start=0, end=len(expr_str)):
 		self.root = None
 		
 		self.validate_parens(expr_str)
 		
-		# Find the first and last characters in the expression, past any outermost parens.
-		# TODO: This will not work! Must locate first outermost operator!!
-		while start < end and start < len(expr_str) and expr_str[start] in [" ", "\t", "("]
-			start += 1
-		while end > start and end > 0 and expr_str[end-1] in [" ", "\t", ")"]
-			end -= 1
+		# Find the last oocurence of an operand in the outermost parenthetical.
+		# This operator has the lowwest precedence. It splits the entire expression in half.
+		close_parens_i = []
+		min_depth = None
+		min_type = None
+		oper_i = 0
 		
-		if start <= end:
-			raise ValueError(f"Empty expression '{expr_str[start:end]}' beginning at position {start+1}..")
+		tag_start = None
+		tag_end = None
 		
-		l_paren_i = find("(", start, end)
-		l_dis_i = expr_str.find("OR", start, end)
-		l_con_i = expr_str.find("AND", start, end)
-		l_neg_i = expr_str.find("NOT", start, end)
+		for i in range(end-1, start-1, -1):
+			if expr_str[i] == ")":
+				close_parens_i.append(i)
+			
+			elif expr_str[i] == "(":
+				if len(close_parens_i) == 0:
+					raise ValueError(f"Unmatched open parenthesis at position {i+1} in '{expr_str[start:end]}'."
+				
+				close_parens_i.pop()
+			
+			# Record the beginning and end of the non-special characters.
+			# If there are no operators, then this string is a single tagname.
+			elif expr_str[i] not in (" ", "\t"):
+				if tag_end is None:
+					tag_end = i+i
+				
+				tag_start = i
+			
+			# Consider looking at operators
+			if min_depth is None or len(close_parens_i) <= min_depth:
+				# For every operator, check if it is present at i.
+				for oper in TagOperator.operations:
+					# Check if remaining operators have priority higher than the lowest yet found at this depth.
+					if min_type is not None and len(close_parens_i) == min_depth and min_type.priority < oper.priority:
+						# All remaining operators have priorities too high to be worth chec
+						break
+					
+					if expr_str.startswith("OR", i):
+						min_depth = len(close_parens_i)
+						min_type = TagDisjunction
+						oper_i = i
 		
-		r_paren_i = rfind(")", start, end)
-		r_dis_i = expr_str.rfind("OR", start, end)
-		r_con_i = expr_str.rfind("AND", start, end)
 		
-		if r_dis_i != -1 and (r_dis_i > r_paren_i or r_paren_i == -1):
-			self.root = TagDisjunction(
-				TagExpression(expr_str, start, r_dis_i),
-				TagExpression(expr_str, r_dis_i+2, end)
-			)
-		
-		elif l_dis_i != -1 and (l_dis_i < l_paren_i or l_paren_i == -1):
-			self.root = TagDisjunction(
-				TagExpression(expr_str, start, l_dis_i),
-				TagExpression(expr_str, l_dis_i+2, end)
-			)
-		
-		elif r_con_i != -1 and (r_con_i > r_paren_i or r_paren_i == -1):
-			self.root = TagConjunction(
-				TagExpression(expr_str, start, r_con_i),
-				TagExpression(expr_str, r_con_i+3, end)
-			)
-		
-		elif l_con_i != -1 and (l_con_i < l_paren_i or l_paren_i == -1):
-			self.root = TagConjunction(
-				TagExpression(expr_str, start, l_con_i),
-				TagExpression(expr_str, l_con_i+3, end)
-			)
-		
-		elif l_neg_i != -1 and (l_neg_i < l_paren_i or l_paren_i == -1):
-			if l_neg_i != start:
-				raise ValueError(f"NOT operator at {l_neg_i+1} must not have preceeding parentheticals or tags; it is a unary operator.")
-		
-		else:
-			raise ValueError(f"Expression '{expr_str[start:end]}' beginning at position {start+1} lacks an operand on both sides of the outermost parentheticals.")
+		# elif l_neg_i != -1 and (l_neg_i < l_paren_i or l_paren_i == -1):
+			# if l_neg_i != start:
+				# raise ValueError(f"NOT operator at {l_neg_i+1} must not have preceeding parentheticals or tags; it is a unary operator.")
 	
 	def validate_parens(self, expr_str, start=0, end=len(expr_str)):
 		paren_indexes = []
