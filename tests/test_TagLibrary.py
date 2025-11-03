2ca882e69dad0a3f0606a381c8a87bc7941a7b09
Digit112 <ekobaddish@gmail.com>
2025-11-03 15:58:27 -0500

    Fixed imports

--- tests/test_TagLibrary.py
+++ tests/test_TagLibrary.py
@@ -1,7 +1,7 @@
 import pytest
 
-from TagManagement.TagLibrary import TagLibrary, TagIntegrityError, TagIdentificationError
-from TagManagement.TagExpression import *
+from ..TagLibrary import TagLibrary, TagIntegrityError, TagIdentificationError
+from ..TagExpression import *
 
 #### Test integrity validation ####
 
